48d7af8a18b0944cc9de924ec48cd65158a970c7
brwong00 <brwong00@gmail.com>
2020-02-23 12:16:28 -0800

    Fixed typos

--- planner/planner/lecture.h
+++ planner/planner/lecture.h
@@ -1,33 +1,31 @@
 #ifndef LECTURE_H
-#define LECUTRE_H
+#define LECTURE_H
 #include <string>
-
-
 
 class Lecture
 {
 public:
-	void setUnit(double unit) { m_unit = unit; }
-	void setLecturer(std::string lecturer) { m_lecturer = lecturer; }
+	// Initializer Functions
 	void setTitle(std::string title) { m_title = title; }
+	void setLecturer(std::string lecturer) { m_lecturer = lecturer; }
+	void setUnit(double unit) { m_unit = unit; }
 	void setTime(int day, std::string time) { m_schedule[day].m_time = time; }
 	void setLocation(int day, std::string location) { m_schedule[day].m_location = location; }
 	void setLength(int day, double length) { m_schedule[day].m_length = length; }
 
-	double getUnit() { return m_unit; }
-	std::string getLecturer() { return m_lecturer; }
+	// Accessor Functions
 	std::string getTitle() { return m_title; }
+	std::string getLecturer() { return m_lecturer; }
+	double getUnit() { return m_unit; }
 	std::string getTime(int day) { return m_schedule[day].m_time; }
 	std::string getLocation(int day) { return m_schedule[day].m_location; }
 	double getLength(int day) { return m_schedule[day].m_length; }
 
 private:
-	double m_unit;
-	std::string m_lecturer;
 	std::string m_title;
+	std::string m_lecturer;
+	double m_unit;
 
-	// 1st is beginning time
-	// 
 	struct schedule {
 		std::string m_time;
 		std::string m_location;

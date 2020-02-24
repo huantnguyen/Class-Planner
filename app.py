32143a5e72a6578f495d404f27eebcdc243416ea
samcho0608 <organicyellow0608@gmail.com>
2020-02-23 20:24:49 -0800

    maybe done?

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 import re
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
 from flask_cors import CORS
 
 # parse json
@@ -16,13 +16,14 @@ ge = []
 # list of classes taken
 class_taken = []
 # TODO: add list of taken classes into list
+dup_classes = { "STATS 10", "STATS 12", "STATS 13", "ECON 41", "PSYCH 100A"}
 
 # list of major requirements
 major_reqs = { "STATS 10", "STATS 12", "STATS 13", "STATS 20", "ECON 41", "PSYCH 100A", "MATH 31A", "MATH 31B",
     "MATH 32A", "MATH 32B", "MATH 33A", "STATS 100A", "STATS 100B", "STATS 100C", "STATS 101A", "STATS 101B",
     "STATS 101C",  "STATS 102A", "STATS 102B", "STATS 102C", "STATS 140SL", "STATS 141SL" }
 # list of non GE majors
-other_majors = { "COM SCI", "STATS", "MATH" }
+# other_majors = { "COM SCI", "STATS", "MATH" }
 
 for i in data:
     # get course num, strip anything after -
@@ -65,19 +66,16 @@ for i in data:
         continue
 
     # ignore classes from 2016-2017 year
-    if course_season == 'F' or course_quarter == "17S":
+    if course_year == '16' or course_quarter == "17S":
         continue
-
-    # create course string with course number, title, professor, and quarter
-    course = course_title + "|" + course_name + "|" + course_prof + "|" + course_quarter
 
     if course_title in major_reqs:
         if course_num < 100:
-            lowerdiv.append(course)
+            lowerdiv.append(i)
         else:
-            upperdiv.append(course)
-    elif course_major not in other_majors:
-        ge.append(course)
+            upperdiv.append(i)
+    else:
+        ge.append(i)
 
 app = Flask(__name__)
 CORS(app)
@@ -89,12 +87,79 @@ def hello_world():
 
 @app.route('/taken')
 def course_taken():
+    dup_taken = False
     class_taken.append(request.args.get("course_taken"))
+    for i in class_taken:
+        if i in dup_classes:
+            dup_taken = True
+
     class_to_be_taken = []
     for i in major_reqs:
         if i not in class_taken:
-            class_to_be_taken.append(i)
-    return str(class_to_be_taken)
+            if i in dup_classes:
+                if dup_taken:
+                    continue
+                else:
+                    class_to_be_taken.append(i)
+            else:
+                class_to_be_taken.append(i)
+    return jsonify({"data": class_to_be_taken})
+
+@app.route('/schedule')
+def recommend_schedule():
+    schedule = [["" for x in range(4)] for y in range(12)]
+    dup_taken = False
+    for i in class_taken:
+        if i in dup_classes:
+            dup_taken = True
+
+    class_to_be_taken = []
+    for i in major_reqs:
+        if i not in class_taken:
+            if i in dup_classes:
+                if dup_taken:
+                    continue
+                else:
+                    class_to_be_taken.append(i)
+            else:
+                class_to_be_taken.append(i)
+
+#    yr = 17
+    majorDone = False
+    count = 0
+    for i in range(12):
+        for j in range(4):
+            if count < len(class_to_be_taken) and not majorDone:
+                schedule[i][j] = class_to_be_taken[count]
+                count = count + 1
+            elif count == len(class_to_be_taken) and not majorDone:
+                majorDone = True
+                count = 0
+                schedule[i][j] = 'GE'
+                count = count + 1
+            elif majorDone:
+                schedule[i][j] = 'GE'
+                count = count + 1
+    return jsonify({"data": schedule})
+
+
+# implement if possible
+'''
+        remainder = i % 3
+        if remainder == 0:
+            szn = 'F'
+        elif remainder == 1:
+            szn = 'W'
+        else:
+            szn = 'S'
+        quarter = str(yr) + szn
+        for j in lowerdiv:
+            course_title = j["courseNum"]
+            course_title = course_title.split('-')[0]
+'''
+
+    
+    
 
 
 
---
+++ check.py
@@ -0,0 +1,2 @@
+schedule = [["" for x in range(4)] for y in range(12)]
+print(schedule[11][3])
